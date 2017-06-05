db121a38017cac719c38c5fb8e5e5d03890ebb4e
TheDeadlyPianist <duane221@yahoo.co.uk>
2017-06-05 17:04:23 +0100

    commit

--- src/main/scala/garage/Customer.scala
+++ src/main/scala/garage/Customer.scala
@@ -1,8 +1,5 @@
 package garage
 
-/**
-	* Created by duane on 05/06/2017.
-	*/
 class Customer (name:String, age:Int) extends Person(name:String, age:Int){
 	
 }
---
+++ src/main/scala/garage/Vehicle.scala
@@ -0,0 +1,8 @@
+package garage
+
+/**
+	* Created by duane on 05/06/2017.
+	*/
+class Vehicle(make:String, model:String, vin:String) {
+	
+}
