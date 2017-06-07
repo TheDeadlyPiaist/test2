dc9baf3bbfcd7e372c1c0d9ea7974fb28e2f05ab
TheDeadlyPianist <duane221@yahoo.co.uk>
2017-06-07 09:44:54 +0100

    commit

--- src/main/scala/primeNumbers/primeNumbers2.scala
+++ src/main/scala/primeNumbers/primeNumbers2.scala
@@ -1,30 +1,35 @@
 package primeNumbers
 
-object primeNumbers1 extends App {
+import primeNumbers.primeNumbers1.{checkedPrimes, start}
+
+object primeNumbers2 extends App {
 	
 	var checkedPrimes = Array[Int]()
 	var start:Int = 5
-	var end:Int = 3000000
+	var end:Int = 2000000000
 	
 	checkedPrimes = checkedPrimes :+ 2
 	checkedPrimes = checkedPrimes :+ 3
 	
-	for (i <- start to end by 2) {
-		var checked:Boolean = false
-		var j:Int = 0
+	var checked:Boolean = false
+	var i:Int = start
+	var j:Int = 0
+	
+	while (i < end) {
+		j = 0
+		checked = false
 		while(!checked && checkedPrimes(j) < Math.sqrt(i)) {
 			j += 1
-			if(i%checkedPrimes(j) == 0) {
-				checked = true
+			i%checkedPrimes(j) match {
+					case 0 => checked = true
+					case _ =>
 			}
 		}
+		i += 2
 		if(!checked) {
 			checkedPrimes = checkedPrimes :+ i
 			println(i)
 		}
-		
 	}
-	
 	println("Finished")
-	
 }
