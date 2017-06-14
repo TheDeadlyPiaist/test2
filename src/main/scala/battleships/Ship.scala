5ad4aa4a5d66bf7dfbc22893e4135b467e112b62
TheDeadlyPianist <duane221@yahoo.co.uk>
2017-06-14 15:07:35 +0100

    commit

--- src/main/scala/battleships/Ship.scala
+++ src/main/scala/battleships/Ship.scala
@@ -5,18 +5,11 @@ abstract class Ship(name:String, length:Int) {
 	private var iLength:Int = length
 	def getName:String = iName
 	def getLength:Int = iLength
-}/*
-object Ship {
-	private class patrolBoat(name:String, length:Int) extends Ship(name, length)
-	private class destroyer(name:String, length:Int) extends Ship(name, length)
-	private class submarine(name:String, length:Int) extends Ship(name, length)
-	private class battleship(name:String, length:Int) extends Ship(name, length)
-	private class aircraftcarrier(name:String, length:Int) extends Ship(name, length)
-	
-	def newPatrolBoat = new patrolBoat("Patrol Boat", 2)
-	def newDestroyer = new destroyer("Destroyer", 3)
-	def newSubmarine = new submarine("Submarine", 3)
-	def newBattleship = new battleship("Battleship", 4)
-	def newAircraftCarrier = new aircraftcarrier("Aircraft Carrier", 5)
 }
-*/
+object Ship {
+	case class patrolBoat(name:String = "Patrol Boat", length:Int = 2) extends Ship(name, length)
+	case class destroyer(name:String = "Destroyer", length:Int = 2) extends Ship(name, length)
+	case class submarine(name:String = "Submarine", length:Int = 3) extends Ship(name, length)
+	case class battleship(name:String = "Battleship", length:Int = 4) extends Ship(name, length)
+	case class aircraftcarrier(name:String = "Aircraft Carrier", length:Int) extends Ship(name, length)
+}
