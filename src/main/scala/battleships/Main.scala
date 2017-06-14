257d59a2538e72b36106c23f6347fc486ac789c3
TheDeadlyPianist <duane221@yahoo.co.uk>
2017-06-14 15:56:20 +0100

    commit

--- src/main/scala/battleships/Main.scala
+++ src/main/scala/battleships/Main.scala
@@ -1,19 +1,32 @@
 package battleships
 
-import scalafx.application
+import scala.collection.mutable.ArrayBuffer
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
-import scalafx.scene.image.Image
+import scalafx.scene.image.{Image, ImageView}
 
 object Main extends JFXApp {
+	
+	var playerShips:ArrayBuffer[Ship] = ArrayBuffer()
+	
 	var myStage:PrimaryStage = new PrimaryStage {
 		title = "Battleships 1.0"
 		
-		
-		scene = new Scene(1000, 800) {
-			content = List()
+		scene = new Scene(1600, 800) {
+			var playerGrid:ImageView = new ImageView(new Image("file:src/images/battleshipgrid.png"))
+			playerGrid.x = 280
+			playerGrid.y = 200
+			var enemyGrid:ImageView = new ImageView(new Image("file:src/images/battleshipgrid.png"))
+			enemyGrid.x = 820
+			enemyGrid.y = 200
+			var bg:ImageView = new ImageView(new Image("file:src/images/wood.png"))
+			bg.fitWidth = 1600
+			bg.fitHeight = 800
+			content = List(bg, playerGrid, enemyGrid)
 		}
+		
+		
 	}
 	
 	var newIcon:Image = new Image("file:src/images/icon.jpg")
--- src/main/scala/battleships/Ship.scala
+++ src/main/scala/battleships/Ship.scala
@@ -8,7 +8,7 @@ abstract class Ship(name:String, length:Int) {
 }
 object Ship {
 	case class patrolBoat(name:String = "Patrol Boat", length:Int = 2) extends Ship(name, length)
-	case class destroyer(name:String = "Destroyer", length:Int = 2) extends Ship(name, length)
+	case class destroyer(name:String = "Destroyer", length:Int = 3) extends Ship(name, length)
 	case class submarine(name:String = "Submarine", length:Int = 3) extends Ship(name, length)
 	case class battleship(name:String = "Battleship", length:Int = 4) extends Ship(name, length)
 	case class aircraftcarrier(name:String = "Aircraft Carrier", length:Int) extends Ship(name, length)
